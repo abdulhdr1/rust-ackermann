67f587d16355da97286bb28795a9fe334a258b56
Abdul Haidar <abdulhdr1@gmail.com>
2023-03-23 23:57:31 -0300

    fix caching layer

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,31 @@ use std::{
 fn main() {
     print!("In A (x,y) \n");
     print!("What's the starting value for x? ");
-    let _ = stdout().flush();
-    let x = get_number();
+    let x = prompt_number();
     print!("What's the starting value for y? ");
-    let _ = stdout().flush();
-    let y = get_number();
+    let y = prompt_number();
     print!("Should it use a caching layer? y/n ");
-    let _ = stdout().flush();
-    let use_cache = use_caching();
+    let use_cache = prompt_cache();
 
     calculate_ackermann(x, y, use_cache);
 }
 
-fn get_number() -> u32 {
+fn prompt_number() -> u32 {
+    let _ = stdout().flush();
+
     let mut s = String::new();
 
     stdin().read_line(&mut s).expect("read error");
 
-    let v = s
-        .split_whitespace()
+    s.split_whitespace()
         .next()
         .unwrap()
         .parse::<u32>()
-        .expect("Pass a valid integer");
-    v
+        .expect("Pass a valid integer")
 }
 
-fn use_caching() -> bool {
+fn prompt_cache() -> bool {
+    let _ = stdout().flush();
     let mut s = String::new();
 
     stdin().read_line(&mut s).expect("read error");
@@ -42,8 +40,13 @@ fn use_caching() -> bool {
 }
 
 fn calculate_ackermann(x: u32, y: u32, cache: bool) -> u32 {
+    let mut v: Option<u32> = None;
+
     if cache {
         match File::open("./cache.txt") {
+            Err(_) => {
+                File::create("./cache.txt").expect("Unable to create file");
+            }
             Ok(mut text) => {
                 let mut buffer = String::new();
 
@@ -54,38 +57,47 @@ fn calculate_ackermann(x: u32, y: u32, cache: bool) -> u32 {
                     .split("\n")
                     .find(|s| s.starts_with(&format!("\"{}-{}\"", x, y)))
                 {
+                    None => {}
                     Some(cache_value) => {
                         println!("Cache hit for A({},{})", x, y);
-                        cache_value.split(":").collect::<Vec<&str>>()[1]
-                            .trim()
-                            .parse::<u32>()
-                            .expect("Invalid cache found");
+                        v = Some(
+                            cache_value.split(":").collect::<Vec<&str>>()[1]
+                                .trim()
+                                .parse::<u32>()
+                                .expect("Invalid cache found"),
+                        );
                     }
-                    None => {}
                 }
-            }
-            Err(_) => {
-                File::create("./cache.txt").expect("Unable to create file");
             }
         }
     }
 
-    println!("Calculating ({}, {})", x, y);
-    let v: u32;
-    match (x, y) {
-        (0, _) => v = y + 1,
-        (_, 0) => v = calculate_ackermann(x - 1, 1, cache),
-        _ => v = calculate_ackermann(x - 1, calculate_ackermann(x, y - 1, cache), cache),
-    }
+    match v {
+        Some(v) => return v,
+        None => {
+            println!("Calculating ({}, {})", x, y);
+            match (x, y) {
+                (0, _) => v = Some(y + 1),
+                (_, 0) => v = Some(calculate_ackermann(x - 1, 1, cache)),
+                _ => {
+                    v = Some(calculate_ackermann(
+                        x - 1,
+                        calculate_ackermann(x, y - 1, cache),
+                        cache,
+                    ))
+                }
+            }
 
-    if cache {
-        let mut file = File::open("./cache.txt").expect("Unable to open file");
-        let mut buffer = String::new();
-        file.read_to_string(&mut buffer)
-            .expect("Unable to read file");
-        buffer.push_str(&format!("\"{}-{}\": {}\n", x, y, v));
-        fs::write("./cache.txt", buffer).expect("Unable to write file");
-    };
-    println!("Found {}, for A({}, {})", v, x, y);
-    v
+            if cache {
+                let mut file = File::open("./cache.txt").expect("Unable to open file");
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer)
+                    .expect("Unable to read file");
+                buffer.push_str(&format!("\"{}-{}\": {}\n", x, y, v.unwrap()));
+                fs::write("./cache.txt", buffer).expect("Unable to write file");
+            };
+            println!("Found {}, for A({}, {})", v.unwrap(), x, y);
+            v.unwrap()
+        }
+    }
 }
